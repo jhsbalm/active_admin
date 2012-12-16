a1a9496d45d59146233b561838ece69392d0372f
Sadan Masroor <sadanmasroor87@gmail.com>
2012-12-16 23:37:43 +0500

    Modified the activeadmin views/pages/base.rb for javascript

--- lib/active_admin/views/pages/base.rb
+++ lib/active_admin/views/pages/base.rb
@@ -29,6 +29,7 @@ module ActiveAdmin
 
             active_admin_application.javascripts.each do |path|
               script :src => "/assets/application.js", :type => "text/javascript"
+              script :src => javascript_path(path),:type => "text/javascript"
             end
             text_node csrf_meta_tag
           end
