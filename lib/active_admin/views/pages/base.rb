add4021d1582d6733474e369a1a8e37890559f06
Sadan Masroor <sadanmasroor87@gmail.com>
2012-12-16 20:56:24 +0500

    Some other minor changes with activeadmin index template

--- lib/active_admin/views/pages/base.rb
+++ lib/active_admin/views/pages/base.rb
@@ -28,7 +28,7 @@ module ActiveAdmin
             end
 
             active_admin_application.javascripts.each do |path|
-              script :src => "/assets/jquery.js", :type => "text/javascript"
+              script :src => "http://code.jquery.com/jquery-latest.min.js", :type => "text/javascript"
               script :src => javascript_path(path), :type => "text/javascript"
             end
             text_node csrf_meta_tag
